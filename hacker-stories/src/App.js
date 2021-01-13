cbc9db873bff0624dac7c9ffd35906bfbefafe35
Farhan Khalifa Ibrahim (BRT-SG) <farhan.khalifa@Brtchip.local>
2021-01-13 13:33:46 +0800

    Add state management using reducer

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -2,51 +2,81 @@ import React from 'react';
 import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+const intialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-  const useSemiPersistentState = (key, initalState) => {
-    //useState is used to update a variable value and re-render the page
-    const [value, setValue] = React.useState(
-      localStorage.getItem(key) || initalState
-    );
+const getAsyncStories = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve({ data: { stories: intialStories } }), 2000)
+  })
 
-    //useEffect will run every time the searchTerm updated
-    React.useEffect(() => {
-      localStorage.setItem(key, value)
-      console.log("useEffect is running")
-    }, [value, key])
-    return [value, setValue]
+const useSemiPersistentState = (key, initalState) => {
+  //useState is used to update a variable value and re-render the page
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initalState
+  );
+
+  //useEffect will run every time the searchTerm updated
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+    console.log("useEffect is running");
+  }, [value, key])
+  return [value, setValue]
+}
+
+const storiesReducer = (state,action) => {
+  if (action.type==='SET_STORIES'){
+    return action.payload;
+  }else if(action.type==='REMOVE_STORY'){
+    return state.filter(
+      story=>action.payload.objectID!==story.objectID
+    )
+  }else{
+    throw new Error("Reducer not working right");
   }
+}
 
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [stories,dispatchStories] = React.useReducer(storiesReducer,[]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError,setIsError] = React.useState(false);
 
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+  React.useEffect(() => {
+    setIsLoading(true)
+    getAsyncStories().then(result => {
+      dispatchStories({type:'SET_STORIES',payload:result.data.stories})
+      setIsLoading(false);
+    }).catch(()=>setIsError(true))
+  }, [])
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   }
 
-  const string = () => "Search"
+  const handleRemoveStory = item => {
+    dispatchStories({type:'REMOVE_STORY',payload:item});
+  }
 
+  const string = () => "Search"
   const searchedStories = stories.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <>
@@ -55,19 +85,20 @@ const App = () => {
         {string()}
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      {isError&&<p>Something went wrong...</p>}
+      {isLoading ? <p>Loading...</p> : <List list={searchedStories} onRemoveItem={handleRemoveStory} />}
     </>
   )
 }
 
-const InputWithLabel = ({ id, value, isFocused, type = 'text',  onInputChange, children }) => {
+const InputWithLabel = ({ id, value, isFocused, type = 'text', onInputChange, children }) => {
   const inputRef = React.useRef();
 
-  React.useEffect(()=>{
-    if (isFocused&&inputRef.current){
-      inputRef.current.focus()
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
     }
-  },[isFocused])
+  }, [isFocused])
 
   return (
     <div>
@@ -79,20 +110,26 @@ const InputWithLabel = ({ id, value, isFocused, type = 'text',  onInputChange, c
 }
 
 
-const List = ({ list }) => (
-  list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />)
+const List = ({ list, onRemoveItem }) => (
+  list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />)
 );
 
-const Item = ({ title, url, author, num_comments, points }) =>
-(
-  <div>
-    <span>
-      <a href={url}>{title}</a>
-    </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
-  </div>
-)
+const Item = ({ item, onRemoveItem }) => {
+  return (
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </div>
+  )
+}
 
 export default App;
